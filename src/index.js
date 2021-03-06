786c4940ff9e261f3c7de5f425333cf8b890b0a9
ignaciosalia <ignaciosalia@gmail.com>
2021-03-06 00:29:23 -0600

    this is weird

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+
+
+class Test extends React.Component {
+  render(){
+    return (
+      <div>
+        <h1 className="thisShit">Hello putos</h1>
+        <p>I don't know what is happening</p>
+      </div>
+
+    );
+  }
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Test />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
